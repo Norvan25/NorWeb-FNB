d1dafdaf8470129072e377e1c4fc68d30c606ff9
Norvan <emil.petrosyan@norvan.io>
2025-12-16 03:45:28 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { CommunicationProvider } from './context/CommunicationContext';
 import { LandingHub } from './pages/LandingHub';
 import { RestaurantPage } from './pages/RestaurantPage';
 import { Rimba } from './pages/Rimba';
@@ -10,16 +11,18 @@ import { Gusto } from './pages/Gusto';
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingHub />} />
-          <Route path="/restaurant/rimba" element={<Rimba />} />
-          <Route path="/restaurant/rouge" element={<Rouge />} />
-          <Route path="/restaurant/veda" element={<Veda />} />
-          <Route path="/restaurant/gusto" element={<Gusto />} />
-          <Route path="/restaurant/:id" element={<RestaurantPage />} />
-        </Routes>
-      </Router>
+      <CommunicationProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingHub />} />
+            <Route path="/restaurant/rimba" element={<Rimba />} />
+            <Route path="/restaurant/rouge" element={<Rouge />} />
+            <Route path="/restaurant/veda" element={<Veda />} />
+            <Route path="/restaurant/gusto" element={<Gusto />} />
+            <Route path="/restaurant/:id" element={<RestaurantPage />} />
+          </Routes>
+        </Router>
+      </CommunicationProvider>
     </CartProvider>
   );
 }
